fc8cad56154bb37ad517bd778dabca5aabb31227
saksham shrestha <yshrestha662@gmail.com>
2022-11-09 12:07:18 +0545

    learn about datatypes

---
+++ CshrapsFundamentals/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+string personname = "bishnu";
+
+//camel casing: personheight, personHeight
+// pascal casing: PersonHeight
+
+// Numbers
+// Integral type
+byte a = 255;
+short b = 32000;
+int c = 100000000;
+long d = 10000000000000000;
+
+// floating point (with decimal points)
+float e = 1231.123f;
+double f = 123456.12345;
+decimal g = 1000000.021522152m;
+
+char h = 'M';
+string i = "saksham shrestha";
+bool j = true;
+ DateTime x = DateTime.Now;
+ Console.WriteLine(x);
+ 
