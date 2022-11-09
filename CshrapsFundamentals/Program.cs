82e4ed8fe7b827ce5eb1d9217805d947e147c559
saksham shrestha <yshrestha662@gmail.com>
2022-11-09 21:52:20 +0545

    assingment_1

--- CshrapsFundamentals/Program.cs
+++ CshrapsFundamentals/Program.cs
@@ -1,26 +1,41 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+//Console.WriteLine("Hello, World!");
 
-string personname = "bishnu";
+//string PersonName = "bishnu";
 
 //camel casing: personheight, personHeight
 // pascal casing: PersonHeight
 
 // Numbers
 // Integral type
-byte a = 255;
-short b = 32000;
-int c = 100000000;
-long d = 10000000000000000;
+//byte a = 255;
+//short b = 32000;
+//int c = 100000000;
+//long d = 10000000000000000;
 
 // floating point (with decimal points)
-float e = 1231.123f;
-double f = 123456.12345;
-decimal g = 1000000.021522152m;
+//float e = 1231.123f;
+//double f = 123456.12345;
+//decimal g = 1000000.021522152m;
 
-char h = 'M';
-string i = "saksham shrestha";
-bool j = true;
- DateTime x = DateTime.Now;
- Console.WriteLine(x);
+//char h = 'M';
+//string i = "saksham shrestha";
+//bool j = true;
+//DateTime x = DateTime.Now;
+//Console.WriteLine(x);
  
+
+
+string employee_Name = "Sakshyam shrestha";
+byte Age =  25;
+string Weight = "75kg";
+bool Passport = true;
+DateTime DateOfBirth = new DateTime(2055, 07, 16);
+
+
+Console.WriteLine(employee_Name);
+Console.WriteLine(Age);
+Console.WriteLine(Weight);
+Console.WriteLine(Passport);
+Console.WriteLine(DateOfBirth);
+Console.WriteLine(employee_Name, Age, Weight, Passport, DateOfBirth);
